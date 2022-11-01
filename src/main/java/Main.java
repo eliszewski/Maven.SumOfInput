f0c67cf08aff84b2c140c4249d7d4ecfae5695a6
Eryk <eryk@Zipcoders-MacBook-Pro-2.local>
2022-11-01 15:58:22 -0400

    tried to make stopwatch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,25 +5,35 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Main {
 
+    public static final double NANOS_PER_SEC = 1000000000.0;
+
 
     public static void main(String[] args){
         System.out.println("Enter a number:");
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
-        System.out.println("Sum is " +sumOfNumbers(number)+ " " + gaussianSumOfNumbers(number));
+        System.out.println("Sum is " +sumOfNumbers(number)+ " and sum was solved by gaussian " + gaussianSumOfNumbers(number));
     }
     
-    public static int sumOfNumbers(int n) {
+    public static String sumOfNumbers(int n) {
+        long start = System.nanoTime();
         int sum = 0;
         while(n>0){
             sum += n;
             n--;
-        } return sum;
+        }
+        long stop = System.nanoTime();
+        long time = (start - stop); /// NANOS_PER_SEC;
+        return "" + sum + " finished in " + time;
     }
     
     // for Extra Credit
-    public static int gaussianSumOfNumbers(int n) {
-        return (n *(n+1)/2);
+    public static String gaussianSumOfNumbers(int n) {
+        long start = System.nanoTime();
+        int sum =(n *(n+1)/2);
+        long stop = System.nanoTime();
+        long time = (start - stop); /// NANOS_PER_SEC;
+        return "" + sum + " finished in " ;
     }
    
     // for Extra extra credit - compare the methods and show which one is faster

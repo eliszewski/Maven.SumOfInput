be007ac6cc5a7b8913df677c3cc9552ff646ed3f
Eryk <eryk@Zipcoders-MacBook-Pro-2.local>
2022-11-01 13:51:55 -0400

    add sumOfInt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,24 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.sql.SQLOutput;
+import java.util.Scanner;
 public class Main {
 
-    public static void main(String[] args){
 
+    public static void main(String[] args){
+        System.out.println("Enter a number:");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        System.out.println("Sum is " +sumOfNumbers(number));
     }
     
-    int sumOfNumbers(int n) {
-        return 0;
+    public static int sumOfNumbers(int n) {
+        int sum = 0;
+        while(n>0){
+            sum += n;
+            n--;
+        } return sum;
     }
     
     // for Extra Credit
